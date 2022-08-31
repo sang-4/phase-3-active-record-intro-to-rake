36a18ed30338deae539793cfbaadc52cfc043843
Allan <kipropsallan@gmail.com>
2022-08-31 17:23:39 +0300

    commit tree 77672d87124f7e518ee4170e01d8bdc0b2339db9

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+namespace :db do
+    desc 'migrate changes to your database'
+    task migrate: :environment do
+      Student.create_table
+    end
+  end
+
 Student.create(name: "Melissa", grade: "10th")
 Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
